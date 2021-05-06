89a80df711e008060a141216e903bad05b6e8553
Rob Hitt <rob.h@taboola.com>
2021-05-06 00:06:11 -0400

    Add additional weather data to the response

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,14 +2,14 @@ const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=263b3f7d4fe0e371e4f96ff3aa0dd5eb&query=${latitude},${longitude}&units=f`;
-  request({url, json: true}, (error, {body}) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service!', undefined)
     } else if (body.error) {
       callback('Unable to find location', undefined);
     } else {
-      const {temperature, feelslike, weather_descriptions} = body.current;
-      const currentWeather = `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`;
+      const { temperature, feelslike, weather_descriptions, wind_speed, precip } = body.current;
+      const currentWeather = `${weather_descriptions[0]} with ${precip}% precipitation. It is currently ${temperature} degrees and feels like ${feelslike} degrees with a windspeed of ${wind_speed} miles/hr.`;
       callback(undefined, currentWeather);
     }
   });
